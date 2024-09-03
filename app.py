153c55675c0962f4592cd8c0f559a5ef7a3194a4
Olga <olga.bimstart@gmail.com>
2024-09-03 12:50:55 +0300

    commit 2

--- app.py
+++ app.py
@@ -1,54 +1,17 @@
-from flask import Flask  # Импортируем класс Flask из библиотеки flask
+from flask import Flask, render_template  # Импортируем класс Flask из библиотеки flask
 
 app = Flask(__name__)  # Создаём экземпляр класса Flask, который будет нашим приложением
 
-@app.route('/test 1')  # Декоратор route определяет маршрут для URL "/"
+@app.route('/test1')  # Декоратор route определяет маршрут для URL "/"
 def fghndf():  # Функция, которая выполняется при обращении к маршруту "/"
-    return """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button>test 1</button>
-</body>
-</html>
-"""  # Возвращает текст "Hello, Flask!" как ответ на запрос
+    return  render_template('hello1.html') 
 
 @app.route('/test2')  # Декоратор route определяет маршрут для URL "/"
 def sgbs():  # Функция, которая выполняется при обращении к маршруту "/"
-    return """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button>test 2</button>
-</body>
-</html>
-"""
+    return render_template('hello2.html') 
 
 @app.route('/test3')  # Декоратор route определяет маршрут для URL "/"
 def jlklh():  # Функция, которая выполняется при обращении к маршруту "/"
-    return """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button>test 3</button>
-    <img src="https://picsum.photos/200" alt="">
-</body>
-</html>
-"""
+    return render_template('hello3.html') 
 if __name__ == '__main__':  # Проверяем, что код запущен как основной модуль, а не импортирован
     app.run(debug=True)  # Запускаем приложение Flask в режиме отладки
